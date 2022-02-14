644193a6d517da99984da442801c7f1cc9ee85ca
OthmanAldefer <74489018+OthmanAldefer@users.noreply.github.com>
2022-02-14 17:22:04 +0300

    Update first_code.cpp

--- first_code.cpp
+++ first_code.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     cout << "hello, world!";
-    cout << "I added this new lines";
+    cout << "This is a first code for my first repository.";
     int a = 0;
     return 0;
     for (int i = 0; i < a; i++)
