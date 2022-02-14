d3a3e9901ecc10823e991f42cf68f60df53ef9d9
OthmanAldefer <oth.aldefer@gmail.com>
2022-02-14 17:12:27 +0300

    add new line

--- first_code.cpp
+++ first_code.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
     cout << "hello, world!";
-    cout << "I added this new line for some change";
+    cout << "I added this new lines";
+    int a = 0;
     return 0;
 }
