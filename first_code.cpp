91cc8e179e470dc9787d2590120ec9781d7f5f2a
OthmanAldefer <oth.aldefer@gmail.com>
2022-02-14 17:23:07 +0300

    make some changes

--- first_code.cpp
+++ first_code.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     cout << "hello, world!";
-    cout << "I added this new lines";
+    cout << "I love you." << endl;
     int a = 0;
     return 0;
     for (int i = 0; i < a; i++)
